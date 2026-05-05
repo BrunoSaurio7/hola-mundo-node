40ac0884e4d0994f63444e0e51024fda529f74a1
Bruno Pérez <160347456+BrunoSaurio7@users.noreply.github.com>
2026-05-04 20:37:08 -0600

    Add event listener to fetch greeting message

--- public/script.js
+++ public/script.js
@@ -1,1 +1,9 @@
+const boton = document.getElementById("btnSaludo");
+const resultado = document.getElementById("resultado");
 
+boton.addEventListener("click", async () => {
+  const response = await fetch("/api/saludo");
+  const data = await response.json();
+
+  resultado.innerHTML = data.mensaje;
+});
